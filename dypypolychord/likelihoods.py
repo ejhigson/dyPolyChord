f1ac9a156f6caa6202ffc96ca8c2ca0315287ace
ejhigson <ejhigson@gmail.com>
2018-03-12 09:36:34 +0000

    adding likelihoods.py

---
+++ dypypolychord/likelihoods.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+Likelihood functions for use with PyPolyChord.
+"""
+import numpy as np
+
+
+def gaussian(theta, sigma=0.5, n_derived=0):
+    """ Simple Gaussian Likelihood centred on the origin."""
+    phi = [0.0] * n_derived
+    dim_theta = len(theta)
+    rad2 = sum([t ** 2 for t in theta])
+    if n_derived != 0:
+        phi[0] = np.sqrt(rad2) * 0
+    logl = -np.log(2 * np.pi * sigma * sigma) * dim_theta / 2.0
+    logl += -rad2 / 2 / sigma / sigma
+    return logl, phi
+
+
+def gaussian_shell(theta, sigma=0.2, rshell=2, n_derived=0):
+    """Gaussian Shell likelihood."""
+    phi = [0.0] * n_derived
+    rad = np.sqrt(sum([t ** 2 for t in theta]))
+    logl = - ((rad - rshell) ** 2) / (2 * sigma ** 2)
+    return logl, phi
+
+
+def rastrigin(theta, n_derived=0, A=10):
+    """Rastrigin Likelihood as described in the PolyChord paper"""
+    phi = [0.0] * n_derived
+    dim_theta = len(theta)
+    ftheta = A * dim_theta
+    for th in theta:
+        ftheta += (th ** 2) - A * np.cos(2 * np.pi * th)
+    logl = -ftheta
+    return logl, phi
+
+
+def rosenbrock(theta, n_derived=0, a=1, b=100):
+    """Rosenbrock Likelihood as described in the PolyChord paper."""
+    phi = [0.0] * n_derived
+    dim_theta = len(theta)
+    ftheta = 0
+    for i in range(dim_theta - 1):
+        ftheta += (a - theta[i]) ** 2
+        ftheta += b * ((theta[i + 1] - (theta[i] ** 2)) ** 2)
+    logl = -ftheta
+    return logl, phi
