37cfd7526267c01c41bd922b3ab93f684f305cf2
ejhigson <ejhigson@gmail.com>
2018-03-31 09:24:40 +0100

    major refactoring of nlives allocation to be clearer and save/print more messages about nlive filtering

--- dyPolyChord/run_dynamic_ns.py
+++ dyPolyChord/run_dynamic_ns.py
@@ -7,13 +7,13 @@ import os
 import time
 import shutil
 import copy
+import itertools
 import numpy as np
 import scipy.signal
 import PyPolyChord
 import nestcheck.io_utils as iou
 import nestcheck.analyse_run as ar
 import nestcheck.data_processing
-# from mpi4py import MPI
 
 
 def run_dypolychord_evidence(pc_settings_in, likelihood, prior, ndims,
@@ -50,17 +50,17 @@ def run_dypolychord_evidence(pc_settings_in, likelihood, prior, ndims,
     PyPolyChord.run_polychord(likelihood, ndims, nderived, pc_settings, prior)
     # Step 2: calculate an allocation of live points
     # ----------------------------------------------
-    nlives_dict = nlive_allocation(pc_settings_in, ninit, nlive_const, 0)
+    dyn_info = nlive_allocation(pc_settings_in, ninit, nlive_const, 0)
     # Step 3: do dynamic run
     # ----------------------
     pc_settings = copy.deepcopy(pc_settings_in)  # remove edits from init
-    pc_settings.nlives = nlives_dict
+    pc_settings.nlives = dyn_info['nlives_dict']
     pc_settings.file_root = pc_settings_in.file_root + '_dyn'
     # In order to start by sampling nlives_array.max() live points but do
     # clustering and resume writing with pc_settings.nlive = ninit we run the
     # first few steps then resume with pc_settings.nlive changed
     pc_settings.seed += 100
-    pc_settings.nlive = max(nlives_dict.values())
+    pc_settings.nlive = max(dyn_info['nlives_dict'].values())
     # ######################################################
     # Commented out save and load due for now to memory leak
     # replaced with below line
@@ -81,7 +81,6 @@ def run_dypolychord_evidence(pc_settings_in, likelihood, prior, ndims,
     # PyPolyChord.run_polychord(likelihood, ndims, nderived, pc_settings, prior)
     # ######################################################
     # Save info about the dynamic run
-    dyn_info = {'nlives': pc_settings.nlives}
     iou.pickle_save(dyn_info,
                     (pc_settings_in.base_dir + '/' +
                      pc_settings_in.file_root + '_dyn_info'),
@@ -95,77 +94,10 @@ def run_dypolychord_evidence(pc_settings_in, likelihood, prior, ndims,
         print('#############################################')
 
 
-def nlive_allocation(pc_settings_in, ninit, nlive_const, dynamic_goal,
-                     **kwargs):
-    """
-    Loads initial run and calculates an allocation of life points for dynamic
-    run.
-    """
-    nodd = (ninit // 2) * 2 + 1  # round ninit up to nearest odd number
-    smoothing_filter = kwargs.pop(
-        'smoothing_filter',
-        lambda x: scipy.signal.savgol_filter(x, nodd, 3, mode='nearest'))
-    if kwargs:
-        raise TypeError('Unexpected **kwargs: {0}'.format(kwargs))
-    assert dynamic_goal in [0, 1]
-    init_run = nestcheck.data_processing.process_polychord_run(
-        pc_settings_in.file_root + '_init', pc_settings_in.base_dir)
-    logx_init = ar.get_logx(init_run['nlive_array'])
-    w_rel = ar.rel_posterior_mass(logx_init, init_run['logl'])
-    # Get importance
-    if dynamic_goal == 0:
-        imp = np.cumsum(w_rel)
-        imp = imp.max() - imp
-        assert imp[0] == imp.max()
-    elif dynamic_goal == 1:
-        imp = w_rel
-    imp /= np.abs(np.trapz(imp, x=logx_init))
-    # calculate a distribution of nlive points in proportion to w_rel
-    if pc_settings_in.max_ndead > 0:
-        samp_remain = pc_settings_in.max_ndead - init_run['logl'].shape[0]
-        assert samp_remain > 0, (
-            'all points used in initial run and none left for dynamic run!')
-    else:
-        samp_remain = init_run['logl'].shape[0] * ((nlive_const / ninit) - 1)
-    nlives_array = imp * samp_remain
-    if smoothing_filter is not None:
-        nlives_array = smoothing_filter(nlives_array)
-    nlives_array = np.rint(nlives_array).astype(int)
-    assert nlives_array.min() >= 0
-    if dynamic_goal == 1:
-        # make sure it does not dip below ninit until it reaches the peak
-        peak_start_ind = np.where(nlives_array >= ninit)[0][0]
-        nlives_array[:peak_start_ind] = ninit
-        nlives_dict = {-1e100: ninit}
-    elif dynamic_goal == 0:
-        assert nlives_array.max() == nlives_array[0]
-        nlives_dict = {-1e100: int(nlives_array.max())}
-    nlives_dict[init_run['logl'][0]] = int(nlives_array[0])
-    # get nlives dict
-    nlives = nlives_array[1:][np.where(np.diff(nlives_array) != 0)]
-    logls = init_run['logl'][1:][np.where(np.diff(nlives_array) != 0)]
-    if dynamic_goal == 0:
-        assert np.all(np.diff(nlives) < 0), (
-            'When targeting evidence, nlive should monotincally decrease!' +
-            'nlives = ' + str(nlives))
-    elif dynamic_goal == 1:
-        turning_points = (np.diff(np.sign(np.diff(nlives))) != 0).sum()
-        if turning_points > 1:
-            print(turning_points, 'turning points in nlive.',
-                  'Perhaps consider using more smoothing?')
-    for i, nlive in enumerate(nlives):
-        nlives_dict[logls[i]] = int(nlive)
-    if dynamic_goal == 0:
-        return nlives_dict
-    else:
-        return nlives_dict, peak_start_ind
-
-
 def run_dypolychord_param(pc_settings_in, likelihood, prior, ndims, **kwargs):
     """
     Dynamic nested sampling targeting increased parameter estimation accuracy
     using polychord.
-
 
     PolyChord checks nlives to see if point should be added or
     removed every single step.
@@ -217,13 +149,12 @@ def run_dypolychord_param(pc_settings_in, likelihood, prior, ndims, **kwargs):
     # Step 2: calculate an allocation of live points
     # ----------------------------------------------
     # calculate a distribution of nlive points in proportion to w_rel
-    nlives_dict, peak_start_ind = nlive_allocation(pc_settings_in, ninit,
-                                                   nlive_const, 1)
+    dyn_info = nlive_allocation(pc_settings_in, ninit, nlive_const, 1)
     # subtract 1 as ndead=1 corresponds to point 0
     resume_steps = np.asarray(step_ndead) - 1
     # Load the last resume before we reach the peak
     resume_ndead = step_ndead[np.where(
-        resume_steps < peak_start_ind)[0][-1]]
+        resume_steps < dyn_info['peak_start_ind'])[0][-1]]
     # copy resume step to dynamic file root
     shutil.copyfile(pc_settings_in.base_dir + '/' + pc_settings_in.file_root +
                     '_init_' + str(resume_ndead) + '.resume',
@@ -239,14 +170,13 @@ def run_dypolychord_param(pc_settings_in, likelihood, prior, ndims, **kwargs):
     pc_settings = copy.deepcopy(pc_settings_in)  # remove edits from init
     pc_settings.seed += 100
     pc_settings.nlive = ninit
-    pc_settings.nlives = nlives_dict
+    pc_settings.nlives = dyn_info['nlives_dict']
     pc_settings.read_resume = True
     pc_settings.file_root = pc_settings_in.file_root + '_dyn'
     PyPolyChord.run_polychord(likelihood, ndims, nderived, pc_settings, prior)
     # Save info about where the dynamic run was resumed from
-    dyn_info = {'resume_ndead': resume_ndead,
-                'resume_nlike': run_outputs_at_resumes[resume_ndead].nlike,
-                'nlives': pc_settings.nlives}
+    dyn_info['resume_ndead'] = resume_ndead
+    dyn_info['resume_nlike'] = run_outputs_at_resumes[resume_ndead].nlike
     iou.pickle_save(dyn_info,
                     (pc_settings_in.base_dir + '/' +
                      pc_settings_in.file_root + '_dyn_info'),
@@ -258,3 +188,98 @@ def run_dypolychord_param(pc_settings_in, likelihood, prior, ndims, **kwargs):
               .format(end_time - start_time))
         print('file_root = ' + pc_settings_in.file_root)
         print('##########################################')
+
+
+# Helper functions
+# ----------------
+
+
+def nlive_allocation(pc_settings_in, ninit, nlive_const, dynamic_goal,
+                     **kwargs):
+    """
+    Loads initial run and calculates an allocation of life points for dynamic
+    run.
+    """
+    nodd = (ninit // 2) * 2 + 1  # round ninit up to nearest odd number
+    smoothing_filter = kwargs.pop(
+        'smoothing_filter',
+        lambda x: scipy.signal.savgol_filter(x, nodd, 3, mode='nearest'))
+    if kwargs:
+        raise TypeError('Unexpected **kwargs: {0}'.format(kwargs))
+    assert dynamic_goal in [0, 1]
+    init_run = nestcheck.data_processing.process_polychord_run(
+        pc_settings_in.file_root + '_init', pc_settings_in.base_dir)
+    logx_init = ar.get_logx(init_run['nlive_array'])
+    w_rel = ar.rel_posterior_mass(logx_init, init_run['logl'])
+    # Calculate the importance of each point
+    if dynamic_goal == 0:
+        imp = np.cumsum(w_rel)
+        imp = imp.max() - imp
+        assert imp[0] == imp.max()
+    elif dynamic_goal == 1:
+        imp = w_rel
+    imp /= np.abs(np.trapz(imp, x=logx_init))
+    assert imp.min() >= 0
+    # Calculate number of samples and multiply it by imp to get nlive
+    if pc_settings_in.max_ndead > 0:
+        samp_remain = pc_settings_in.max_ndead - init_run['logl'].shape[0]
+        assert samp_remain > 0, (
+            'all points used in initial run and none left for dynamic run!')
+    else:
+        samp_remain = init_run['logl'].shape[0] * ((nlive_const / ninit) - 1)
+    nlives_unsmoothed = imp * samp_remain
+    # Smooth and round nlive
+    if smoothing_filter is not None:
+        # Perform smoothing *before* rounding nlives_unsmoothed
+        nlives = np.rint(smoothing_filter(nlives_unsmoothed))
+    else:
+        nlives = np.rint(nlives_unsmoothed)
+    nlives_unsmoothed = np.rint(nlives_unsmoothed)
+    if dynamic_goal == 1:
+        # make sure nlives does not dip below ninit until it reaches the peak
+        peak_start_ind = np.where(nlives >= ninit)[0][0]
+        nlives[:peak_start_ind] = ninit
+        # for diagnostics on smooothing, lets give the unsmoothed nlives the
+        # same treatment
+        nlives_unsmoothed[:np.where(nlives_unsmoothed >= ninit)[0][0]] = ninit
+    # Get the indexes of nlives points which are different to the previous
+    # points (i.e. remove consecutive duplicates, keeping first occurance)
+    inds_to_use = np.concatenate((np.asarray([0]),
+                                  np.where(np.diff(nlives) != 0)[0] + 1))
+    # ################################################
+    # Perform some checks
+    assert (count_turning_points(nlives) ==
+            count_turning_points(nlives[inds_to_use]))
+    if dynamic_goal == 0:
+        assert nlives[0] == nlives.max()
+        assert np.all(np.diff(nlives) < 0), (
+            'When targeting evidence, nlive should monotincally decrease!'
+            + ' nlives = ' + str(nlives))
+    elif dynamic_goal == 1:
+        assert nlives[0] == ninit
+        msg = str(count_turning_points(nlives)) + ' turning points in nlive.'
+        if smoothing_filter is None:
+            msg += ' Smoothing_filter is None.'
+        else:
+            msg += ' Without smoothing_filter there would have been '
+            msg += str(count_turning_points(nlives_unsmoothed))
+        print(msg)
+    # ################################################
+    # Check logl = approx -inf is mapped to the starting number of live points
+    nlives_dict = {-1e100: nlives[0]}
+    for ind in inds_to_use:
+        nlives_dict[init_run['logl'][ind]] = int(nlives[ind])
+    # Store the nlive allocations for dyn_info
+    dyn_info = {'init_nlive_allocation': nlives,
+                'init_nlive_allocation_unsmoothed': nlives_unsmoothed,
+                'nlives_dict': nlives_dict}
+    if dynamic_goal == 1:
+        dyn_info['peak_start_ind'] = peak_start_ind
+    return dyn_info
+
+
+def count_turning_points(array):
+    """Returns number of turning points in a 1d array."""
+    # remove adjacent duplicates only
+    uniq = np.asarray([k for k, g in itertools.groupby(array)])
+    return (np.diff(np.sign(np.diff(uniq))) != 0).sum()
