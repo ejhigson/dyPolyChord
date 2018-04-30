5befb97591c35c058a486d2424e075e3cc5af00d
ejhigson <ejhigson@gmail.com>
2018-04-30 13:00:20 +0100

    MPI working for dynamic_goal=0

--- dyPolyChord/run_dynamic_ns.py
+++ dyPolyChord/run_dynamic_ns.py
@@ -89,6 +89,12 @@ def run_dypolychord(run_func, dynamic_goal, **kwargs):
     default_smoothing = (lambda x: scipy.signal.savgol_filter(
         x, 1 + (2 * ninit), 3, mode='nearest'))
     smoothing_filter = kwargs.pop('smoothing_filter', default_smoothing)
+    comm = kwargs.pop('comm', None)
+    if comm is not None:
+        rank = comm.Get_rank()
+    else:
+        rank = 0
+    print("comm =", comm, 'rank =', rank)
     if kwargs:
         raise TypeError('Unexpected **kwargs: {0}'.format(kwargs))
     assert not settings_dict_in['nlives']
@@ -129,63 +135,69 @@ def run_dypolychord(run_func, dynamic_goal, **kwargs):
                             root + '_init_' + str(step_ndead[-1]) + '.resume')
     # Step 2: calculate an allocation of live points
     # ----------------------------------------------
-    init_run = nestcheck.data_processing.process_polychord_run(
-        settings_dict_in['file_root'] + '_init', settings_dict_in['base_dir'])
-    # Calculate max number of samples
-    if settings_dict_in['max_ndead'] > 0:
-        samp_tot = settings_dict_in['max_ndead']
-        assert settings_dict_in['max_ndead'] > init_run['logl'].shape[0], (
-            'all points used in initial run and none left for dynamic run!')
-    else:
-        samp_tot = init_run['logl'].shape[0] * (nlive_const / ninit)
-        assert nlive_const > ninit
-    dyn_info = dyPolyChord.nlive_allocation.allocate(
-        init_run, samp_tot, dynamic_goal, smoothing_filter=smoothing_filter)
-    if dyn_info['peak_start_ind'] != 0:
-        # subtract 1 as ndead=1 corresponds to point 0
-        resume_steps = np.asarray(step_ndead) - 1
-        print('resume_steps:', resume_steps, 'peak_start_ind:',
-              dyn_info['peak_start_ind'])
-        # Work out which resume file to load
-        resume_ndead = step_ndead[np.where(
-            resume_steps < dyn_info['peak_start_ind'])[0][-1]]
-        # copy resume step to dynamic file root
-        shutil.copyfile(root + '_init_' + str(resume_ndead) + '.resume',
-                        root + '_dyn.resume')
-        # Save resume info
-        dyn_info['resume_ndead'] = resume_ndead
-        dyn_info['resume_nlike'] = outputs_at_resumes[resume_ndead]['nlike']
-    nestcheck.io_utils.pickle_save(
-        dyn_info, root + '_dyn_info', overwrite_existing=True)
-    try:
-        # Remove all the temporary resume files. Use set to avoid duplicates as
-        # these cause OSErrors.
-        for snd in set(step_ndead):
-            os.remove(root + '_init_' + str(snd) + '.resume')
-    except NameError:  # occurs when not saving resumes so step_ndead list
-        pass
-    # Step 3: do dynamic run
-    # ----------------------
-    final_init_seed = settings_dict['seed']
-    settings_dict = copy.deepcopy(settings_dict_in)  # remove edits from init
-    if final_init_seed >= 0:
-        assert settings_dict_in['seed'] >= 0, (
-            'if input seed was <0 it should not have been edited')
-        settings_dict['seed'] = final_init_seed + seed_increment
-    if dyn_info['peak_start_ind'] != 0:
-        settings_dict['nlive'] = ninit
-    else:
-        settings_dict['nlive'] = dyn_info['nlives_dict'][
-            min(dyn_info['nlives_dict'].keys())]
-    settings_dict['nlives'] = dyn_info['nlives_dict']
-    settings_dict['read_resume'] = (dyn_info['peak_start_ind'] != 0)
-    settings_dict['file_root'] = settings_dict_in['file_root'] + '_dyn'
-    run_func(settings_dict)
-    # tidy up remaining .resume files (if the function has reach this point,
-    # both the initial and dynamic runs have finished so we shouldn't need to
-    # resume)
-    for extra in ['init', 'dyn']:
+    if rank == 0:
+        init_run = nestcheck.data_processing.process_polychord_run(
+            settings_dict_in['file_root'] + '_init', settings_dict_in['base_dir'])
+        # Calculate max number of samples
+        if settings_dict_in['max_ndead'] > 0:
+            samp_tot = settings_dict_in['max_ndead']
+            assert settings_dict_in['max_ndead'] > init_run['logl'].shape[0], (
+                'all points used in initial run and none left for dynamic run!')
+        else:
+            samp_tot = init_run['logl'].shape[0] * (nlive_const / ninit)
+            assert nlive_const > ninit
+        dyn_info = dyPolyChord.nlive_allocation.allocate(
+            init_run, samp_tot, dynamic_goal, smoothing_filter=smoothing_filter)
+        if dyn_info['peak_start_ind'] != 0:
+            # subtract 1 as ndead=1 corresponds to point 0
+            resume_steps = np.asarray(step_ndead) - 1
+            print('resume_steps:', resume_steps, 'peak_start_ind:',
+                  dyn_info['peak_start_ind'])
+            # Work out which resume file to load
+            resume_ndead = step_ndead[np.where(
+                resume_steps < dyn_info['peak_start_ind'])[0][-1]]
+            # copy resume step to dynamic file root
+            shutil.copyfile(root + '_init_' + str(resume_ndead) + '.resume',
+                            root + '_dyn.resume')
+            # Save resume info
+            dyn_info['resume_ndead'] = resume_ndead
+            dyn_info['resume_nlike'] = outputs_at_resumes[resume_ndead]['nlike']
+        nestcheck.io_utils.pickle_save(
+            dyn_info, root + '_dyn_info', overwrite_existing=True)
         try:
-            os.remove(root + '_{0}.resume'.format(extra))
-        except FileNotFoundError:
+            # Remove all the temporary resume files. Use set to avoid duplicates as
+            # these cause OSErrors.
+            for snd in set(step_ndead):
+                os.remove(root + '_init_' + str(snd) + '.resume')
+        except NameError:  # occurs when not saving resumes so step_ndead list
             pass
+        # Step 3: do dynamic run
+        # ----------------------
+        final_init_seed = settings_dict['seed']
+        settings_dict = copy.deepcopy(settings_dict_in)  # remove edits from init
+        if final_init_seed >= 0:
+            assert settings_dict_in['seed'] >= 0, (
+                'if input seed was <0 it should not have been edited')
+            settings_dict['seed'] = final_init_seed + seed_increment
+        if dyn_info['peak_start_ind'] != 0:
+            settings_dict['nlive'] = ninit
+        else:
+            settings_dict['nlive'] = dyn_info['nlives_dict'][
+                min(dyn_info['nlives_dict'].keys())]
+        settings_dict['nlives'] = dyn_info['nlives_dict']
+        settings_dict['read_resume'] = (dyn_info['peak_start_ind'] != 0)
+        settings_dict['file_root'] = settings_dict_in['file_root'] + '_dyn'
+    if comm is not None:
+        print("comm is not None! bcasting.")
+        comm.bcast(settings_dict, root=0)
+        comm.barrier()
+    run_func(settings_dict)
+    if rank == 0:
+        # tidy up remaining .resume files (if the function has reach this point,
+        # both the initial and dynamic runs have finished so we shouldn't need to
+        # resume)
+        for extra in ['init', 'dyn']:
+            try:
+                os.remove(root + '_{0}.resume'.format(extra))
+            except FileNotFoundError:
+                pass
