e466651786f0c9813f593a7c771b56ffe347fa65
ejhigson <ejhigson@gmail.com>
2018-05-12 16:33:04 +0100

    minor doc edit

--- docs/conf.py
+++ docs/conf.py
@@ -49,9 +49,9 @@ extensions = [
 ]
 
 
-# Needed to avoid errors in class member docstrings
-# For more information see https://stackoverflow.com/questions/12206334/sphinx-autosummary-toctree-contains-reference-to-nonexisting-document-warnings
-numpydoc_show_class_members = False
+# # Needed to avoid errors in class member docstrings
+# # For more information see https://stackoverflow.com/questions/12206334/sphinx-autosummary-toctree-contains-reference-to-nonexisting-document-warnings
+# numpydoc_show_class_members = False
 
 # autodoc defaults
 # For more information see http://www.sphinx-doc.org/en/master/ext/autodoc.html
